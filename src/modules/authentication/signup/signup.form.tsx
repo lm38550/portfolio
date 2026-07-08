5419705b91ef01c7354c343fd4d51ec9911ce55c
Morel Louis <louis.morel@etu.univ-lyon1.fr>
2026-07-08 14:12:05 +0200

    Fixing box and deactivating react hook form

--- src/modules/authentication/signup/signup.form.tsx
+++ src/modules/authentication/signup/signup.form.tsx
@@ -1,7 +1,7 @@
-import {useForm} from "react-hook-form";
+// import {useForm} from "react-hook-form";
 
 export const SignupForm = () => {
-    const {handleSubmit, control, formState: {errors}, register, setError, reset,} = useForm()
+    // const {handleSubmit, control, formState: {errors}, register, setError, reset,} = useForm()
     return (
         <>
             formulaire
