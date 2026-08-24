00c692827535070e3bc5c8445603babdee6192ce
MOREL Louis <lm38550@gmail.com>
2026-08-24 22:12:40 +0200

    Adding "no image" & new projects

--- src/ui/components/project-presentation/project-links.tsx
+++ src/ui/components/project-presentation/project-links.tsx
@@ -41,27 +41,41 @@ export const ProjectList : ProjectLinks[] = [
         tags : ["C++", "GKit"]
     },
     {
-        title: "?????",
-        description: "?????",
-        gitUrl: "https://github.com/lm38550/mif37_am",
-        pageUrl: "@/app/projects/MIF37-AM",
-        imageUrl: "/img/projects/MIF37-AM.png",
-        imageAlt: "Image de présentation du projet : Sythèse d'image - Mouvements d'un robot",
-        compatible_mac : true,
-        compatible_linux : true,
+        title: "Budget Manager",
+        description: "ATTENTION CE LOGICIEL EST EN COURS DE REFONTE TOTALE \n " +
+            "Logiciel de gestion de budget",
+        gitUrl: "https://github.com/lm38550/BudgetManager",
+        pageUrl: "@/app/projects/BudgetManager",
+        imageUrl: "",
+        imageAlt: "Image de présentation du projet : Budget Manager",
+        compatible_mac : false,
+        compatible_linux : false,
         compatible_windows : true,
-        tags : ["C++", "GKit"]
+        tags : ["Pyhton", "SQLlite", "C++", "WxWidget"]
     },
     {
-        title: "?????",
-        description: "?????",
-        gitUrl: "https://github.com/lm38550/mif37_am",
-        pageUrl: "@/app/projects/MIF37-AM",
-        imageUrl: "/img/projects/MIF37-AM.png",
-        imageAlt: "Image de présentation du projet : Sythèse d'image - Mouvements d'un robot",
+        title: "L'Incroyable Fermier Assassin Partant Checher Des Abricots (LIFAPCDA)",
+        description: "Projet universitaire de développement de logiciel. LIFAPCDA est un jeu similaire aux premier jeu Zelda, " +
+            "avec pour objectif de récupérer des abricots et tuer des slimes pour gagner.",
+        gitUrl: "https://github.com/lm38550/LIFAPCDA",
+        pageUrl: "@/app/projects/LIFAPCDA",
+        imageUrl: "",
+        imageAlt: "Image de présentation du projet : LIFAPCDA",
         compatible_mac : true,
         compatible_linux : true,
         compatible_windows : true,
-        tags : ["C++", "GKit"]
-    }
+        tags : ["C++", "SDL2"]
+    },
+    {
+        title: "Gestionnaire de CV",
+        description: "Projet universitaire de développement et gestion de projet. Ce gestionnaire permet de trier des CV en fonction de différents critères et d'en calculer le score",
+        gitUrl: "https://github.com/lm38550/mif01_cv",
+        pageUrl: "@/app/projects/MIF01_CV",
+        imageUrl: "",
+        imageAlt: "Image de présentation du projet : Gestionnaire de CV",
+        compatible_mac : true,
+        compatible_linux : true,
+        compatible_windows : true,
+        tags : ["Java", "Maven"]
+    },
 ]
--- src/ui/components/project-presentation/project-presentation.tsx
+++ src/ui/components/project-presentation/project-presentation.tsx
@@ -15,11 +15,15 @@ export const Projects = () => {
     const projectsList = ProjectList.map((project) => (
         <div className="items-center border-2 grid grid-cols-1 md:grid-cols-4 rounded min-h-60 mt-5 p-2 md:gap-3 bg-day-200 dark:bg-night-200" key={uuidv4()}>
             <div className="h-full min-h-40 w-full md:col-span-1 relative rounded">
-                <Image src={project.imageUrl} alt={project.imageAlt} fill className="rounded object-cover"/>
+                {project.imageUrl === "" ?
+                    <Image src="/img/no-image.png" alt="Image indiquant qu'il n'y a pas d'image pour ce projet" fill className="rounded object-cover"/>
+                    :
+                    <Image src={project.imageUrl} alt={project.imageAlt} fill className="rounded object-cover"/>
+                }
             </div>
             <div className="md:col-span-3 flex items-center md:items-start flex-col gap-3">
                 <Typography variant="h3" component="h1" className="text-center md:text-start">{project.title}</Typography>
-                <Typography variant="body-sm" component="p" className="text-center md:text-start md:ml-3">{project.description}</Typography>
+                <Typography variant="body-sm" component="p" className="text-center md:text-start md:ml-3 whitespace-pre-line">{project.description}</Typography>
                 <div className="flex items-center gap-2">
                     <div className="flex border-r-2 border-day-800 dark:border-night-800 pr-1">
                         {project.compatible_windows && <RiWindowsFill size={30} className="text-day-800 dark:text-night-800" />}
@@ -27,7 +31,7 @@ export const Projects = () => {
                         {project.compatible_mac && <RiAppleFill size={30} className="text-day-800 dark:text-night-800" />}
                     </div>
                     {project.tags.map((tag) => (
-                        <Typography variant="caption-sm" component="p"
+                        <Typography variant="caption-sm" component="p" key={uuidv4()}
                                     className="bg-day-400 dark:bg-night-400 p-2 rounded">
                             {tag}
                         </Typography>
