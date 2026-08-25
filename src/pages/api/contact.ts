42cad268a587e4e3b5b38e7a2a1bc3b818a83014
MOREL Louis <lm38550@gmail.com>
2026-08-25 12:37:25 +0200

    Editing mail content

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -13,8 +13,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             from: 'contact@louismorel.dev',
             to: 'lm38550@gmail.com',
             subject: `New form contact : ${object}`,
-            html: '<p>Hey ! ${name} <${email}> sent you a contact form</p>' +
-                '${content}',
+            html: `<p>Hey !</p>
+                <strong>${name} &lt;${email}&gt; ${number}</strong> à remplis un formulaire sur le Portfolio</p>
+                ${how_did_know !== "" ? `<p>Vous a connu via : ${how_did_know}</p>` : ""}
+                <p><strong>-----</strong></p>
+                <div style="border: 1px solid #ccc; border-radius: 8px; padding: 16px; margin: 16px 0; background-color: #f9f9f9;">
+                    ${content}
+                </div>
+                <p><strong>-----</strong></p>`,
         })
         console.log(send)
     } catch (err: any) {
