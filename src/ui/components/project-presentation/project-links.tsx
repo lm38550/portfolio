33f85805ce1d8c209d57833cb0c50a39b85ca655
MOREL Louis <lm38550@gmail.com>
2026-08-29 14:37:04 +0200

    Adding new projects and images

--- src/ui/components/project-presentation/project-links.tsx
+++ src/ui/components/project-presentation/project-links.tsx
@@ -15,6 +15,33 @@ export const ProjectList : ProjectLinks[] = [
         tags : ["C++", "GKit"]
     },
     {
+        title: "Floutage de l'arrière plan en temps réel",
+        description: "Projet universitaire d'analyse d'image. Dans ce projet à but expérimental nous avons essayé plusieurs approches " +
+            "pour flouter en temps réel l'arrière plan d'une video (par exemple dans une visioconférence) sans utiliser de machine learning.",
+        gitUrl: "https://github.com/lm38550/mif17-analyse",
+        pageUrl: "@/app/projects/MIF17_ANALYSE",
+        imageUrl: "/img/projects/MIF17-analyse.png",
+        imageAlt: "Image de présentation du projet : Floutage de l'arrière plan en temps réel",
+        compatible_mac : true,
+        compatible_linux : true,
+        compatible_windows : true,
+        tags : ["Python", "OpenCV"]
+    },
+    {
+        title: "Simulation de tissu jacquard",
+        description: "Projet universitaire d'ouverture à la recherche. En colaboration avec un enseignant-chercheur " +
+            "nous avons suivi un de ses projets de recherche. Nous avons développé une application accessible et intuitive " +
+            "pour le musée des soiries simulant la construction d'un tissu.",
+        gitUrl: "https://github.com/llikian/simulation-tissus-jacquard",
+        pageUrl: "@/app/projects/jacquard",
+        imageUrl: "/img/projects/JACQUARD.png",
+        imageAlt: "Image de présentation du projet : Simulation de tissu jacquard",
+        compatible_mac : true,
+        compatible_linux : true,
+        compatible_windows : true,
+        tags : ["JavaScript", "VueJS"]
+    },
+    {
         title: "Sythèse d'image - Mouvements d'un robot",
         description: "Projet Universitaire réalisé en 2026, mettant en scène un robot. L'objectif était de faire l'animation de " +
             "ce robot",
@@ -26,6 +53,32 @@ export const ProjectList : ProjectLinks[] = [
         compatible_linux : true,
         compatible_windows : true,
         tags : ["C++", "GKit"]
+    },
+    {
+        title: "Synthèse d'image - Parametrisation d'un mesh",
+        description: "Projet universitaire de synthèse d'image. En récupérant un mesh 3D, notre objectif était de pouvoir appliquer " +
+            "une texture 2D grâce à la paramétrisation",
+        gitUrl: "https://github.com/lm38550/mif27-parameterization",
+        pageUrl: "@/app/projects/MIF27_PARAMETERIZATION",
+        imageUrl: "/img/projects/MIF27-Parameterization.png",
+        imageAlt: "Image de présentation du projet : Parametrisation d'un mesh",
+        compatible_mac : true,
+        compatible_linux : true,
+        compatible_windows : true,
+        tags : ["C++", "GKit3"]
+    },
+    {
+        title: "L'Incroyable Fermier Assassin Partant Checher Des Abricots (LIFAPCDA)",
+        description: "Projet universitaire de développement de logiciel. LIFAPCDA est un jeu similaire aux premier jeu Zelda, " +
+            "avec pour objectif de récupérer des abricots et tuer des slimes pour gagner.",
+        gitUrl: "https://github.com/lm38550/LIFAPCDA",
+        pageUrl: "@/app/projects/LIFAPCDA",
+        imageUrl: "/img/projects/LIFAPCDA.png",
+        imageAlt: "Image de présentation du projet : LIFAPCDA",
+        compatible_mac : true,
+        compatible_linux : true,
+        compatible_windows : true,
+        tags : ["C++", "SDL2"]
     },
     {
         title: "Sythèse d'image - Mouvements d'une créature soumise à des forces physiques",
@@ -54,28 +107,42 @@ export const ProjectList : ProjectLinks[] = [
         tags : ["Pyhton", "SQLlite", "C++", "WxWidget"]
     },
     {
-        title: "L'Incroyable Fermier Assassin Partant Checher Des Abricots (LIFAPCDA)",
-        description: "Projet universitaire de développement de logiciel. LIFAPCDA est un jeu similaire aux premier jeu Zelda, " +
-            "avec pour objectif de récupérer des abricots et tuer des slimes pour gagner.",
-        gitUrl: "https://github.com/lm38550/LIFAPCDA",
-        pageUrl: "@/app/projects/LIFAPCDA",
-        imageUrl: "",
-        imageAlt: "Image de présentation du projet : LIFAPCDA",
-        compatible_mac : true,
-        compatible_linux : true,
-        compatible_windows : true,
-        tags : ["C++", "SDL2"]
-    },
-    {
         title: "Gestionnaire de CV",
         description: "Projet universitaire de développement et gestion de projet. Ce gestionnaire permet de trier des CV en fonction de différents critères et d'en calculer le score",
         gitUrl: "https://github.com/lm38550/mif01-cv",
         pageUrl: "@/app/projects/MIF01_CV",
-        imageUrl: "",
+        imageUrl: "/img/projects/MIF01-CV.png",
         imageAlt: "Image de présentation du projet : Gestionnaire de CV",
         compatible_mac : true,
         compatible_linux : true,
         compatible_windows : true,
         tags : ["Java", "Maven"]
+    },
+    {
+        title: "Gestionnaire de commande d'une pizzeria",
+        description: "Projet universitaire de développement d'application web. Nous avons réalisé en binome, un site web " +
+            "permettant à une pizzeria de gérer ses commandes, et à un clients de passer des commandes, avec la possibilité de les " +
+            "personnaliser.",
+        gitUrl: "https://github.com/lm38550/mif03-web",
+        pageUrl: "@/app/projects/mif03_web",
+        imageUrl: "/img/projects/MIF03-web.png",
+        imageAlt: "Image de présentation du projet : Gestionnaire de commande d'une pizzeria",
+        compatible_mac : true,
+        compatible_linux : true,
+        compatible_windows : true,
+        tags : ["Java", "Maven", "Tomcat", "Spring"]
+    },
+    {
+        title: "Analyse d'image",
+        description: "Projet universitaire d'analyse d'image. Nous avons effectués de simple opérations d'analyse d'image : " +
+            "histogramme, égalisation, inversion, extension, compression, ...",
+        gitUrl: "https://github.com/lm38550/mif02-analyse",
+        pageUrl: "@/app/projects/mif02_analyse",
+        imageUrl: "/img/projects/MIF02-analyse.png",
+        imageAlt: "Image de présentation du projet : Analyse d'image",
+        compatible_mac : true,
+        compatible_linux : true,
+        compatible_windows : true,
+        tags : ["C++", "OpenCV"]
     },
 ]
